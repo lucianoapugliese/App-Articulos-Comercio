8273d3d68433cea621f418bcd548695ba1579a51
Facundo Amarilla <facundogabriel91@gmail.com>
2022-09-10 01:36:32 -0300

    Primer Commit

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Articulo
+    {
+        // Atributos:
+        public int _codArticulo { get; set; }
+        public string _nombre { get; set; } 
+        public string _descripcion { get; set; }
+        public string _urlImagen { get; set; }
+        public decimal _precio { get; set; }
+    }
+}
---
+++ Dominio/Elemento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Elemento
+    {
+        public int _Id { get; set; }
+        public string _Descripcion { get; set; }
+    }
+}
